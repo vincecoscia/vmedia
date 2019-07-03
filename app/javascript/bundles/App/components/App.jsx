2b1c7a8b0245f1ce3ba50d76327575a3babb1467
Vincent Coscia <xvincentcosciax@gmail.com>
2019-07-03 15:25:13 -0400

    Added material UI and react router dom

--- app/javascript/bundles/App/components/App.jsx
+++ app/javascript/bundles/App/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hi
+        <Navbar/>
+        <div>Hi</div>
       </div>
     )
   }
---
+++ app/javascript/bundles/App/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import PhotoIcon from '@material-ui/icons/Photo';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import PublicIcon from '@material-ui/icons/Public';
+import HomeIcon from '@material-ui/icons/Home';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu" onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <PhotoIcon />
+              </ListItemIcon>
+              <ListItemText primary="Photos" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <PublicIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" />
+            </StyledMenuItem>
+          </StyledMenu>
+          <Typography variant="h6" className={classes.title}>
+            VMedia
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
