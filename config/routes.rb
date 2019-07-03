30024102639e8e8060d1319d9a1803639a6c48b4
Vincent Coscia <xvincentcosciax@gmail.com>
2019-07-03 10:37:36 -0400

    Added index and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
+
+  root 'pages#index'
+
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
